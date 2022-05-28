893b97df98d95161cbc83a3b87e7f96e0ee5a028
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-29 00:32:49 +0200

    order proyects

---
+++ test/CalculatorLibraryTests/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using CalculatorLibrary;
+using Xunit;
+
+namespace CalculatorLibraryTests;
+
+public class CalculatorTests
+{
+
+    private Calculator _calculator = new();
+
+    [Fact]
+    public void TestAdd() 
+        => Assert.Equal(9, _calculator.Add(5, 4));
+
+    [Fact]
+    public void TestSubtract()
+        => Assert.Equal(1, _calculator.Subtract(5, 4));
+    
+
+    [Fact]
+    public void TestMultiply()
+        => Assert.Equal(20, _calculator.Multiply(5, 4));
+    
+    [Fact]
+    public void TestDivide()
+        => Assert.Equal(2, _calculator.Divide(10, 5));
+
+}
