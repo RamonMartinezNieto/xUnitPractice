aa2bfd6a17240310735aecb7531727868628aa1e
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-28 12:54:28 +0200

    ADd Calculator library

---
+++ CalculatorLibrary/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculatorLibrary;
+
+public class Calculator
+{
+
+    public static int Add (int firstNumber, int secondNumber)
+        => firstNumber + secondNumber;
+
+    public static int Subtract(int firstNumber, int secondNumber)
+        => firstNumber - secondNumber;
+
+    public static int Multiply(int firstNumber, int secondNumber)
+        => firstNumber * secondNumber;
+
+    public static int Divide(int firstNumber, int secondNumber)
+        => firstNumber / secondNumber;
+
+}
