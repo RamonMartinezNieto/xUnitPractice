5b794c3daf8561191bae2728cf8152856b38977b
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-29 01:02:12 +0200

    constructor as Setup and implememnt IDispose as a teardown

--- test/CalculatorLibraryTests/CalculatorTests.cs
+++ test/CalculatorLibraryTests/CalculatorTests.cs
@@ -1,10 +1,21 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
 
 namespace CalculatorLibrary.Tests.Unit;
 
-public class CalculatorTests
+public class CalculatorTests : IDisposable
 {
     private readonly Calculator _sut = new(); //System under test - sut 
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    //Setup, in xUnit constructor is using as a Setup
+    public CalculatorTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+
+        _testOutputHelper.WriteLine("Hello from the constructor."); //ctor = constructor
+    }
 
     [Fact]
     public void Add_ShouldAddTwoNumbers_WhenToNumbersAreIntegers()
@@ -34,7 +45,17 @@ public class CalculatorTests
     public void Divide_ShouldDivideTwoNumbers_WhenToNumbersAreIntegers()
         => Assert.Equal(2, _sut.Divide(10, 5));
 
+    //Dispose is the teardown
+    public void Dispose()
+    {
+        _testOutputHelper.WriteLine("Hello from the cleanup."); 
+    }
 }
 
 //Note: for every single tests xUnit creates a new instance of the private readonly parameter
 //sut (system under test) this helps us because every tests is self contained.
+
+//xunit uses C# way to write the setup and the teardown (cleanup) using the constructor and the dispose
+
+//If we have an async tests we can implement IAsyncLifetime and implemente InitializeAsync and DisposeAsync 
+//to implement Setup and Teardown, ctor run first than InitializeAsync, it is important to remember. 
