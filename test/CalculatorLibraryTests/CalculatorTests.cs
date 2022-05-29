11f9de6f4d0dd07c8ee4fb1808e69c0892f3f103
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-29 19:49:36 +0200

    Add basic exercises

--- test/CalculatorLibraryTests/CalculatorTests.cs
+++ test/CalculatorLibraryTests/CalculatorTests.cs
@@ -17,7 +17,6 @@ public class CalculatorTests : IDisposable
         _testOutputHelper.WriteLine("Hello from the constructor."); //ctor = constructor
     }
 
-    [Fact]
     public void Add_ShouldAddTwoNumbers_WhenToNumbersAreIntegers()
     {
         //Arrange
@@ -31,7 +30,7 @@ public class CalculatorTests : IDisposable
     }
 
 
-    [Theory(Skip = "Skip a complete Theory")]
+    [Theory(Skip = "Skip a complete Theory")] //This is not commont and maybe is an error to do that
     [InlineData(0,0,0, Skip = "I can skip a InlineData! :)")]
     [InlineData(5,4,1)]
     [InlineData(5,5,0)]
@@ -57,6 +56,51 @@ public class CalculatorTests : IDisposable
     {
         _testOutputHelper.WriteLine("Hello from the cleanup."); 
     }
+
+
+
+    ///// ******************************************* /////
+    ///// ********* Exercise of the course. ********* /////
+    ///// ******************************************* /////
+    
+    [Theory]
+    [InlineData(5, 5, 10)]
+    [InlineData(-5, 5, 0)]
+    [InlineData(-15, (-5), -20)]
+    public void AddExercise_ShoulAddTwoNumbers_WhenNUmbersAreIntegers(
+        int numberOne, int numberTwo, int expected)
+        => Assert.Equal(expected, _sut.Add(numberOne, numberTwo));
+    
+    [Theory]
+    [InlineData(5, 5, 0)]
+    [InlineData(15, 5, 10)]
+    [InlineData(-5, (-5), 0)]
+    [InlineData(-15, (-5), -10)]
+    [InlineData(5, 10, -5)]
+    public void SubtractExercise_ShoulSubtractTwoNumbers_WhenNUmbersAreIntegers(
+        int numberOne, int numberTwo, int expected)
+        => Assert.Equal(expected, _sut.Subtract(numberOne, numberTwo));  
+    
+    [Theory]
+    [InlineData(5, 5, 25)]
+    [InlineData(50, 0, 0)]
+    [InlineData(-5, 5, -25)]
+    public void MultiplyExercise_ShoulMultiplyTwoNumbers_WhenNUmbersAreIntegers(
+        int numberOne, int numberTwo, int expected)
+        => Assert.Equal(expected, _sut.Multiply(numberOne, numberTwo));
+    
+        
+    [Theory]
+    [InlineData(5, 5, 1)]
+    [InlineData(15, 5, 3)]
+    public void DivideExercise_ShoulDivideTwoNumbers_WhenNUmbersAreIntegers(
+        int numberOne, int numberTwo, int expected)
+        => Assert.Equal(expected, _sut.Divide(numberOne, numberTwo));
+    
+    
+    ///// ******************************************* /////
+    ///// ******* End Exercise of the course. ******* /////
+    ///// ******************************************* /////
 }
 
 //Note: for every single tests xUnit creates a new instance of the private readonly parameter
