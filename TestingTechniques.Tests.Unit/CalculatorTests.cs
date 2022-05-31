27f00e9faa4bfbdf23b930172df6e0f34755cc7e
RamónMN <ramon.martinez.nieto@gmail.com>
2022-05-31 22:01:00 +0200

    add test to check exceptions

---
+++ TestingTechniques.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace TestingTechniques.Tests.Unit;
+
+public class CalculatorTests
+{
+    private readonly Calculator _sut = new Calculator();
+
+    [Fact]
+    public void ExceptionThrownAssertionExample() 
+    {
+        Action result = () => _sut.Divide(1, 0); //can be an Action or Func
+
+        result.Should()
+            .Throw<DivideByZeroException>()
+            .WithMessage("Attempted to divide by zero.");
+    }
+}
---
+++ TestingTechniques/Calculator.cs
@@ -0,0 +1,27 @@
+﻿namespace TestingTechniques;
+
+public class Calculator
+{
+
+    public int Add (int firstNumber, int secondNumber)
+        => firstNumber + secondNumber;
+
+    public int Subtract(int firstNumber, int secondNumber)
+        => firstNumber - secondNumber;
+
+    public int Multiply(int firstNumber, int secondNumber)
+        => firstNumber * secondNumber;
+
+    public int Divide(int firstNumber, int secondNumber)
+    {
+        EnsureThatDivisorIsNotZero(secondNumber);
+
+        return firstNumber / secondNumber;
+    }
+
+    private static void EnsureThatDivisorIsNotZero(int secondNumber)
+    {
+        if(secondNumber == 0)
+            throw new DivideByZeroException("Attempted to divide by zero.");
+    }
+}
