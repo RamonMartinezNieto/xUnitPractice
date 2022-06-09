fcd312234969a8ca6d6bd753317df40424252764
RamónMN <ramon.martinez.nieto@gmail.com>
2022-06-09 19:45:38 +0200

    add constructor and dispose in fixture

--- AdvanceTechniques.Tests.Unit/MyClassFixture.cs
+++ AdvanceTechniques.Tests.Unit/MyClassFixture.cs
@@ -1,6 +1,18 @@
 ﻿namespace AdvanceTechniques.Tests.Unit;
 
-public class MyClassFixture
+public class MyClassFixture : IDisposable
 {
     public Guid Id { get; } = Guid.NewGuid();
+
+    public MyClassFixture(Guid id)
+    {
+        //This will runs once before any test in the class that implement it
+        //remember that the constructor in the class tests is running before each test
+    }
+
+    public void Dispose()
+    {
+        //This will runs once at the end of all tests
+        //remember that the dispose in the class tests is running after each test
+    }
 }
